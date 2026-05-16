df914523e40a1346cf4d559738b271ecca63c27d
Вормя :p <167223943+BadCoder228@users.noreply.github.com>
2026-05-17 03:09:31 +0700

    Update main.py -- ver 1.1.0

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from os import getlogin, system, popen
 from colorama.ansi import AnsiFore
-from os import getlogin, system
 from es3_modifier import ES3
 from colorama import Fore
 from time import sleep
@@ -10,6 +10,7 @@ from sys import exit
 LE_PATH: str = f"C:/Users/{getlogin()}/AppData/LocalLow/semiwork/Repo/MetaSave.es3"
 LE_PASSWORD: str = "Why would you want to cheat?... :o It's no fun. :') :'D"
 LE_BEAUTIFUL_CONSOLE: str = f"C:\\\\{getlogin()} >>> "
+APP: str = "REPO.exe"
 
 def BeautifulString(word: str) -> str:
     return f"you need to type \"{word}\" in the console below."
@@ -26,6 +27,12 @@ def Banner() -> None:
     BannerPattern(Fore.MAGENTA, "R.E.P.O.   STYLIST", 
         f"\nIn order to unlock cosmetic items, {BeautifulString('meow')}\nIn order to leave, {BeautifulString('done')}\n\n(C) This program was written and developed by {Fore.YELLOW}Wormyy{Fore.WHITE}, no rights were reserved")
 
+def DetectIfTheGameIsLaunched() -> None:
+    if APP in popen('tasklist').read():
+        print(f"{Fore.CYAN}[Notice]{Fore.WHITE} An active Repo instance was found, closing it...")
+        system(f"taskkill /f /im {APP} > NUL 2>&1")
+        print(f"{Fore.GREEN}[Success!]{Fore.WHITE} The instance was destroyed!\n")  
+
 def Inject() -> None:
     try:
         with open(LE_PATH, 'rb') as f:
@@ -37,10 +44,10 @@ def Inject() -> None:
         with open(LE_PATH, 'wb') as f:
             f.write(file_data.save(dumps(data)))
 
-        print(f"\n{Fore.GREEN}[Success!]{Fore.WHITE} You may now close the window.\n")
+        print(f"\n\n{Fore.GREEN}[Success!]{Fore.WHITE} You may now close the window.\n")
         
     except Exception as e:
-        print(f"\n{Fore.RED}[An error occurred!]{Fore.WHITE} Here's the error: {e}. Try to fix it. You may now close the window.\n")
+        print(f"\n\n{Fore.RED}[An error occurred!]{Fore.WHITE} Here's the error: {e}. Try to fix it. You may now close the window.\n")
     
     input(LE_BEAUTIFUL_CONSOLE)
     exit(0)
@@ -57,7 +64,9 @@ if __name__ == "__main__":
 
             case "meow":
                 print("\n")
-                                
+                
+                DetectIfTheGameIsLaunched()
+
                 for i in range(10): #imitating the process... looks beautiful in my eyes, ngl
                     print(f"Hold on{'.' * i}", end="\r")
                     sleep(0.1)
